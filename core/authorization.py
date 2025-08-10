4293e8af6585c6f187b9481bb699bdb3ae853481
Ivan Tsybulin <tsybulinhome@gmail.com>
2025-08-10 20:55:36 +0300

    implement auth using openfga sdk

--- core/authorization.py
+++ core/authorization.py
@@ -1,12 +1,35 @@
 from django.conf import settings
+from openfga_sdk.client import ClientConfiguration, ClientCheckRequest
+from openfga_sdk.sync import OpenFgaClient
 
 
 class AuthorizationClient:
     def __init__(self):
-        self.store_id = settings.OPENFGA_STORE_ID
-        self.model_id = settings.OPENFGA_MODEL_ID
+        store_id = settings.OPENFGA_STORE_ID
+        model_id = settings.OPENFGA_MODEL_ID
+
+        if not store_id:
+            self.client = None
+            return
+
+        configuration = ClientConfiguration(
+            api_url="http://openfga:8080",
+            store_id=store_id,
+            authorization_model_id=model_id,
+        )
+
+        self.client = OpenFgaClient(configuration)
 
     def check(self, uid, relation, domain):
-        if not self.store_id:
+        if not self.client:
             return True
-        return False
+
+        body = ClientCheckRequest(
+            user=f"user:{uid}",
+            relation=relation,
+            object=f"domain:{domain}",
+        )
+
+        response = self.client.check(body)
+
+        return response.allowed
